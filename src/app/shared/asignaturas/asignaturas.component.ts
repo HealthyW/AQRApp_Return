76bdaa72b24e5d057513b784ea21985294075610
Cristian Gutierrez <134895358+HealthyW@users.noreply.github.com>
2024-09-23 02:38:34 -0300

    asignaturas component base lista
    falta que se expanda al hacer click

--- src/app/shared/asignaturas/asignaturas.component.ts
+++ src/app/shared/asignaturas/asignaturas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-asignaturas',
@@ -10,5 +10,8 @@ export class AsignaturasComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
+  @Input() spanText: string = 'Clase sin nombre';
+  @Input() divClass: string = 'custom-div';
 
 }
