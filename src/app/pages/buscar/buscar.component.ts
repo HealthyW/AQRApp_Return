36baf668955839a22649d76ff8056ea9088b7115
Cristian Gutierrez <134895358+HealthyW@users.noreply.github.com>
2024-09-28 17:46:17 -0300

    pagina buscar listo

--- src/app/pages/buscar/buscar.component.ts
+++ src/app/pages/buscar/buscar.component.ts
@@ -11,4 +11,23 @@ export class BuscarComponent  implements OnInit {
 
   ngOnInit() {}
 
+  public data = [
+    'PGY4121_001D',
+    'PY41447_004D',
+    'ASY4131_001D',
+    'INI5111_003D',
+    'CSY4111_001D',
+    'INI5111_001D',
+    'MAT4140_003D',
+    'EAY4450_001D',
+    'PGY4121_002D',
+    'INI5111_002D',
+  ];
+  public results = [...this.data];
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
+  }
+
 }
