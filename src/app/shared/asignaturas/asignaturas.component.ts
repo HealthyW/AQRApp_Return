69fadd243dbda14d9361657b9001c4917878a81d
Cristian Gutierrez <134895358+HealthyW@users.noreply.github.com>
2024-10-16 01:18:12 -0300

    qr listo

--- src/app/shared/asignaturas/asignaturas.component.ts
+++ src/app/shared/asignaturas/asignaturas.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from 'src/app/servicios/auth.service';
+import QRious from 'qrious';
 
 @Component({
   selector: 'app-asignaturas',
@@ -11,6 +12,9 @@ export class AsignaturasComponent  implements OnInit {
   profesor:boolean = false;
   isVisible = false;
   isVisibleQr = false;
+  qrData: string = '';
+
+  @ViewChild('qrCanvas') qrCanvas!: ElementRef<HTMLCanvasElement>;
 
   @Input() spanText: string = 'Clase sin nombre';
   @Input() divClass: string = 'custom-div';
@@ -27,8 +31,35 @@ export class AsignaturasComponent  implements OnInit {
     this.isVisible = !this.isVisible;
   }
 
+  /*
   activateDivQr(){
     this.isVisibleQr = true;
+  }*/
+
+  generarQR() { 
+    const fechaActual = new Date();
+    // Formatear la fecha con guiones
+    const año = fechaActual.getFullYear();
+    const mes = String(fechaActual.getMonth() + 1).padStart(2, '0'); // Los meses son de 0 a 11, por eso sumamos 1
+    const día = String(fechaActual.getDate()).padStart(2, '0');
+    // Formatear la hora con :
+    const horas = String(fechaActual.getHours()).padStart(2, '0');
+    const minutos = String(fechaActual.getMinutes()).padStart(2, '0');
+    const segundos = String(fechaActual.getSeconds()).padStart(2, '0');
+    
+    const fechaHora = `${año}-${mes}-${día}_${horas}:${minutos}:${segundos}`;
+    this.qrData = `http://localhost:8100/asistencia/${this.spanText}/${fechaHora}`;
+
+    this.createQR();
+    this.isVisibleQr = true;
+  }
+
+  createQR() {
+    const qr = new QRious({
+      element: this.qrCanvas.nativeElement,
+      value: `Asignatura ID: ${this.spanText}`,
+      size: 200
+    });
   }
 
 }
